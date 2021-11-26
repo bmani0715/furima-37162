06061e500b5bc2ee024dea0ad8caec4b4d88ae99
bmani0715 <bzmania0715@yahoo.co.jp>
2021-11-26 19:40:56 +0900

    任意項目の修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   #has_many :orders
 
   with_options presence: true do
-    validates :nickname, uniqueness: true
+    validates :nickname
 
     PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
     validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
