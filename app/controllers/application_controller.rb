3bd53bd2e1b042e4cd34dae6408c6d90551208ee
bmani0715 <bzmania0715@yahoo.co.jp>
2021-11-26 16:42:34 +0900

    ユーザー管理機能テストコードの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
-
   before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:last_name,:first_name,:last_name_kana,:first_name_kana,:birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
   end
 
   def basic_auth
@@ -13,5 +14,4 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
     end
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :items
-         has_many :orders
- 
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :last_name_kana, presence: true
-         validates :first_name_kana, presence: true
-         validates :birthday, presence: true
-         
+  has_many :items
+  has_many :orders
+
+  with_options presence: true do
+    validates :nickname, uniqueness: true
+    validates :email, uniqueness: true
+
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end
