d86caa3aabd2fb29479c40faa1e59ddc3112c1b1
bmani0715 <bzmania0715@yahoo.co.jp>
2021-11-29 19:05:54 +0900

    テストコードの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,30 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+
   def index
+    #@items = Item.includes(:user).order('created_at DESC')
   end
 
+  def new
+    @item = Item.new
+  end
 
-
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
-  def set_item
-    @item = Item.find(params[:id])
-  end
+  #def set_item
+  #  @item = Item.find(params[:id])
+  #end
 
   def item_params
     params.require(:item).permit(:product_name,:description,:category_id,:product_condition_id,:delivery_fee_id,:prefecture_id,:days_to_ship_id,:price,:image).merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,25 +1,47 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
   belongs_to :user
   has_one :order
   has_one_attached :image
   belongs_to_active_hash :category
-  belongs_to_active_hash :sales_status
-  belongs_to_active_hash :shipping_fee_status
+  belongs_to_active_hash :product_condition
+  belongs_to_active_hash :delivery_fee
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :scheduled_delivery
+  belongs_to_active_hash :days_to_ship
+
+  #with_options presence: true do
+  #  validates :product_name, :description,:image
+    
+  #  with_options numericality: { other_than: 1 } do
+  #    validates :category_id, :product_condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id
+  #  end
+
+    
+  #  with_options format: { with: /\A[0-9]+\z/ } do
+  #   validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
+  #  end
+  #end
 
   with_options presence: true do
-    validates :title, :text, :image
-    
-    with_options numericality: { other_than: 1 } do
-      validates :category_id, :sales_status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id
-    end
-
-    with_options format: { with: /\A[0-9]+\z/ } do
-      validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
-    end
+    validates :image
+    validates :product_name
+    validates :description
+    validates :category_id
+    validates :product_condition_id
+    validates :delivery_fee_id
+    validates :prefecture_id
+    validates :days_to_ship_id
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
+  with_options numericality: { other_than: 1 , message: "can't be blank"} do
+    validates :category_id
+    validates :prefecture_id
+    validates :product_condition_id
+    validates :delivery_fee_id
+    validates :days_to_ship_id
+  end
 
 end
