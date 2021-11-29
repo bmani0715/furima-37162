39fef3293c434f3b9f153c5d3fbfc2fb01a35db6
bmani0715 <bzmania0715@yahoo.co.jp>
2021-11-29 21:54:06 +0900

    商品出品機能の修正#1

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
 
   belongs_to :user
-  has_one :order
+  #has_one :order
   has_one_attached :image
   belongs_to_active_hash :category
   belongs_to_active_hash :product_condition
@@ -18,11 +18,6 @@ class Item < ApplicationRecord
   #    validates :category_id, :product_condition_id, :delivery_fee_id, :prefecture_id, :days_to_ship_id
   #  end
 
-    
-  #  with_options format: { with: /\A[0-9]+\z/ } do
-  #   validates :price, numericality: { greater_than: 300, less_than: 9999999 } 
-  #  end
-  #end
 
   with_options presence: true do
     validates :image
@@ -33,7 +28,7 @@ class Item < ApplicationRecord
     validates :delivery_fee_id
     validates :prefecture_id
     validates :days_to_ship_id
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}
   end
 
   with_options numericality: { other_than: 1 , message: "can't be blank"} do
