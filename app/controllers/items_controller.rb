92b24cd6b2c484b492d98af988c4aad077431222
bmani0715 <bzmania0715@yahoo.co.jp>
2021-12-01 16:49:21 +0900

    Privateメソッド中の処理をbeforeaction〜へ統合

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,14 +24,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
     unless user_signed_in? && current_user.id == @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
        redirect_to item_path(item_params)
     else
