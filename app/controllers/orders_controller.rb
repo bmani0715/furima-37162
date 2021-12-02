8ab30eeae8247dd32bcbabb4ac07fd42f41438c2
bmani0715 <bzmania0715@yahoo.co.jp>
2021-12-02 16:21:11 +0900

    アクションの実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,9 +2,17 @@ class OrdersController < ApplicationController
 
 
     def index
+      @item_order = ItemOrder.new
     end
   
     def create
+      @item_order = ItemOrder.new(order_params)
+      if @item_order.valid?
+        @item_order.save
+        return redirect_to root_path
+      else
+        render : :index
+      end
     end
   
 
