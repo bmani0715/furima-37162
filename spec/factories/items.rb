17349d352a7f13dfbdb34c35704ea8f2864f79ae
bmani0715 <bzmania0715@yahoo.co.jp>
2021-12-06 18:11:10 +0900

    レビュー修正#3

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -4,12 +4,12 @@ FactoryBot.define do
 
     product_name               {Faker::Name.name}
     description                {Faker::Lorem.sentence}
-    category_id                {'2'}
-    product_condition_id       {'2'}
-    delivery_fee_id            {'2'}
-    prefecture_id              {'2'}
-    days_to_ship_id            {'2'}
-    price                      {'1000'}  
+    category_id                {2}
+    product_condition_id       {2}
+    delivery_fee_id            {2}
+    prefecture_id              {2}
+    days_to_ship_id            {2}
+    price                      {1000}  
     
 
     after(:build) do |item|
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item_order do
     token          {'tok_abcdefghijk00000000000000000'}
     postal_code    {'123-4567'}
-    prefecture_id  {'2'}
+    prefecture_id  {2}
     municipalities {'横浜市緑区'}
     block_number   {'青山1-1-1'}
     building_name  {'柳ビル103'}
